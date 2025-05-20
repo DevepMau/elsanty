4a954796d34074ebf03341b9eea3c8516faf5725
DevepMau <oshiettecs@gmail.com>
2025-05-19 23:02:12 -0300

    Agregamos un logo al NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './NavBar.css';
 import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
 
 export default function NavBar() {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">elsanty</div>
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" />
+      </div>
       <ul className="navbar-links">
         <li><Link to="/">Inicio</Link></li>
         <li><Link to="/about">Sobre Nosotros</Link></li>
