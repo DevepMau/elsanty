68fd57b0f553026a9e993fcf0c5df05a5c48db6e
DevepMau <oshiettecs@gmail.com>
2025-05-25 02:32:38 -0300

    Se ha puesto una imagen en el home de fondo - se agrego texto basico - se alieno el texto y el imageSlider uno al lado del otro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './Home.css';
 import ImageSlider from '../components/ImageSlider';
 
 export default function Home() {
   return (
-    <div>Home
-      <ImageSlider />
+    <div className='home-container'>
+      <div className="home-content">
+        <div className="text-section">
+          <h1>Nombre del Emprendimiento</h1>
+          <p>Tu lema o frase inspiradora aquí</p>
+        </div>
+        <div className="slider-section">
+          <ImageSlider />
+        </div>
+      </div>
+      <div className='overlay'></div>
     </div>
-  )
+  );
 }
